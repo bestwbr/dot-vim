b8e3b8bbd2b88d316af931311bbad9d531fce4a2
Jason Wang <1724555125@qq.com>
2019-05-09 18:22:16 +0800

    Auto-install all plugins

--- install.sh
+++ install.sh
@@ -6,7 +6,12 @@ if [[ ! -e ~/.vim/autoload/plug.vim ]]; then
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
+cd ~
+type nvim > /dev/null 2>&1 && rm -rf .config/nvim && ln -s ~/.vim .config/nvim
+type nvim > /dev/null 2>&1 && nvim +PlugInstall +qall || vim +PlugInstall +qall
+
 # compile YouCompleteMe
 cd ~/.vim/plugged/YouCompleteMe
-./install.sh --clang-completer --rust-completer
+type cargo > /dev/null 2>&1 && RUST="--rust-completer" || RUST=""
+./install.sh --clang-completer $RUST
 cd -
